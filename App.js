45382203931efccbc6c0f368133f49521de7eddd
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-12 22:15:20 +0530

    learning with JSX in react

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "It's an H1 tag"),
-    React.createElement("h2", {}, "It's an H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "It's an H1 tag"),
-    React.createElement("h2", {}, "It's an H2 tag"),
-  ]),
-]);
+// React.createElement => Object => HTMLElement(render)
 
-// const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
-// h1 => h1 is the tag which decides the heading
-// {} => attribute to the tag ex: {id:"heading"} which later on be used to perform some actions by css
-// message => message which needs to be printed with h1 tag
-// <h1> Hello world from react </h1>
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // creating root element
+console.log(heading);
 
-root.render(parent); // that's how you render the elements on react
-console.log(parent);
+// JSX - HTML-like or XML-like syntax (JSX code is transpiled before it reached to the JS Engine) - PARCEL - Babel(do the transpilation)
+const jsxHeading = (
+  <h1 className="heading" tabIndex="1">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+console.log(jsxHeading);
+
+console.log(heading === jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
