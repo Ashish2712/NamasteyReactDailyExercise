5dd59a71b34cd6c7164246f12f88f19d9d40349c
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-21 17:33:38 +0530

    added MENU_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL = "https://png.pngtree.com/png-clipart/20220628/original/pngtree-food-logo-png-image_8239850.png";
+export const MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.2124007&lng=78.1772053&restaurantId=";
