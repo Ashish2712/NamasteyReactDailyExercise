e85c3c0959b503f30d602741c94852f1fc0981d3
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-21 17:35:00 +0530

    created About.js
    This file contains header page About

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>Welcome to the new series of React!</h2>
+        </div>
+    );
+};
+
+export default About;
