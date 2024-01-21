33b008aecfbe0cbfbfec23d92b8df3ff19fe0a82
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-21 17:31:36 +0530

    added react-router-dom
    Learned about how react-router-dom works and how it routes through your app

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import Body from "./Body";
-
-const RestaurantCard = (props) => {
-  const {resData} = props;
-  
-  const {
-    cloudinaryImageId,
-    name,
-    avgRating,
-    cuisines,
-    costForTwo,
-    deliveryTime,
-  } = resData?.info;
-
-  return (
-    <div className="res-card" style={{backgroundColo: "#f0f0f0"}}>
-    <img className="res-card-logo" alt="res-logo" 
-      src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}/>
-      <h3>{name}</h3>
-      <h4>{cuisines.join(" , ")}</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{deliveryTime}</h4>
-    </div>
-  );
-};
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
+import RestaurantsMenu from "./RestaurantsMenu";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantsMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
