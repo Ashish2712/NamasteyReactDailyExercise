11e449c16b77120c9ee42cd284532e492405efe9
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-22 21:48:14 +0530

    added user.js file to save user details

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>Welcome to the new series of React!</h2>
+            <User/>
         </div>
     );
 };
