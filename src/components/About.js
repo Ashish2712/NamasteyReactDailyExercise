38903ae8085ca39374347b722f4b12e5f646ee39
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-23 20:50:59 +0530

    props in class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,15 @@
 import User from "./User";
 import UserClass from "./UserClass";
 
-
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>Welcome to the new series of React!</h2>
-            <User/>
-            <UserClass />
-        </div>
-    );
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>Welcome to the new series of React!</h2>
+      <User name={"Ashish Singh (function)"} />
+      <UserClass  name={"Ashish Singh (class)"}/>
+    </div>
+  );
 };
 
 export default About;
