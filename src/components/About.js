2f893918f5529107f9e685e3c7317f23b196fc01
Ashish singh kushwah <akushwah206@gmail.com>
2024-01-24 21:43:45 +0530

    componentDidMount in class based components

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,32 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>Welcome to the new series of React!</h2>
-      <User name={"Ashish Singh (function)"} />
-      <UserClass  name={"Ashish Singh (class)"}/>
-    </div>
-  );
-};
+class About extends Component{
+
+    constructor(props){
+        super(props);
+
+        console.log("Parent constructor is called!")
+    }
+
+
+componentDidMount(){
+    console.log("Parent component did mount is called!");
+}
+
+render(){
+    console.log("parent render is called!")
+    return (
+        <div>
+          <h1>About</h1>
+          <h2>Welcome to the new series of React!</h2>
+          {/* <User name={"Ashish Singh (function)"} /> */}
+          <UserClass  name={"Ashish Singh (class)"}/>
+        </div>
+      );
+}
+}
+
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,16 +6,29 @@ class UserClass extends React.Component{
         super(props)
 
         this.state = {
-            count : 0
+            count : 0,
         }
+
+        console.log("child constructor is called!")
+    }
+
+    componentDidMount(){
+        console.log("Child componentDidMount is called!")
     }
 
     render() {
+        console.log("Child render is called!");
         const {name} = this.props;
-        const {count} = this.state
+        const {count, count2} = this.state
         return(
             <div className="user-card">
                 <h1>Count: {count}</h1>
+                <button onClick={() =>{
+                // NEVER UPDATE STATE VARIABLE DIRECTLY
+                this.setState(
+                    {count: this.state.count + 1}
+                );
+                }}>Count Increase</button>
                 <h2>Name: {name}</h2>
                 <h3>Location: Noida </h3>
                 <h4>Social Media: Not available</h4>
